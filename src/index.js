1379fa21ed53954fed92b1e18ce79b2c4e32c4f3
Niko Bec <nikobec96@gmail.com>
2020-11-11 17:53:15 +0100

    added note jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
 
